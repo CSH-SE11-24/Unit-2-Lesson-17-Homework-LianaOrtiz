f081f7a23c603f2515da635968427bad8bc8a01c
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 12:08:54 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const prompt = require('prompt-sync')()
+// Create an object that represents your favorite restaurant with the keys name (string), cuisine (string), and rating (number)
+let restaurant = {
+  name: "smashburger",
+  cuisine: "american",
+  rating: 4.1,
+}
+
+// Console log the restaurant's name using the object
+
+console.log(restaurant.name)
+
+// Add a new key called location and add the restaurant's location
+ restaurant.location = "4325 Boston Rd"
+
+
+// Change the rating of the restaurant to increase it by one
+
+restaurant.rating = 4.2
+
+// Add a new key called test and set the value to null
+
+
+
+// Delete the test key
+
+
+
+// Console log the object to see if the test key is gone
+console.log(restaurant)
+
+
+
+// Loop through the object and print every key and element.
+// It should look like: 
+// name is Ugly Baby
+// cuisine is thai
+// rating is 10
+// location is 407 Smith St, Brooklyn
+
+
+
+
+
